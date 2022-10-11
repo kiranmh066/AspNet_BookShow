7b765753343c936c86bc83c41362e048a30263c3
Kiran Mahalingappa Hegganadavara <Kiran.MH@valtech.com>
2022-10-11 18:22:08 +0530

    git commit

---
+++ BookShow/BookShowDAL/BookShow.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using BookShowEntity;
+
+namespace BookShowDAL
+{
+    public class BookShowDbContext:DbContext
+    {
+        public BookShowDbContext(DbContextOptions<BookShowDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Movie> movies { get; set; }
+        public DbSet<Theatre> theatre { get; set; }
+        public DbSet<ShowTiming> showTiming { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlServer("Data Source=VDC01LTC2164; Initial Catalog = BookShow; Integrated Security=True;");
+        }
+    }
+}
---
+++ BookShow/MovieAppCoreAPI/Controllers/MovieController.cs
@@ -0,0 +1,48 @@
+﻿using BookShowBLL.Services;
+using BookShowEntity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MovieAppCoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private MovieService _movieService;
+
+        public MovieController(MovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        [HttpGet("GetMovies")]//
+        public IEnumerable<Movie> GetMovies()
+        {
+            return _movieService.GetMovies();
+        }
+
+        [HttpPost("AddMovie")]
+        public IActionResult AddMovie([FromBody] Movie movie)
+        {
+            _movieService.AddMovie(movie);
+            return Ok("Movie created Successfully");
+        }
+
+        [HttpDelete("DeleteMovie")]
+        public IActionResult DeleteMovie([FromBody] int movieId)
+        {
+            _movieService.DeleteMovie(movieId);
+            return Ok("Movie deleted Successfully");
+        }
+
+        [HttpPut("UpdateMovie")]
+        public IActionResult UpdateMovie([FromBody] Movie movie)
+        {
+            _movieService.UpdateMovie(movie);
+            return Ok("Movie Updated Successfully");
+        }
+    }
+}
